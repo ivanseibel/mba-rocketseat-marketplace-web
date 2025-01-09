8b117917b1c7170d077c5c37410c6bdb36a29100
Ivan Luis Seibel <ivanseibel@gmail.com>
2025-01-09 18:42:52 +0000

    Add authentication routes and update router configuration

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,24 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 import { AppLayout } from "./_layouts/app";
+import { AuthLayout } from "./_layouts/auth";
 import { Dashboard } from "./pages/app/dashboard";
-// import { AppLayout } from "./_layouts/app";
-// import { AuthLayout } from "./_layouts/auth";
-// import { Orders } from "./pages/app/orders/orders";
-// import { SignIn } from "./pages/auth/sign-in";
-// import { SignUp } from "./pages/auth/sign-up";
-// import { Error } from "./pages/error";
-// import { NotFound } from "./pages/not-found";
+import { Products } from "./pages/app/dashboard/products";
+import { SignIn } from "./pages/auth/sign-in";
+import { ErrorPage } from "./pages/error";
 
 export const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <AppLayout />,
-		// errorElement: <Error />,
-		errorElement: <div>Error</div>,
+		errorElement: <ErrorPage />,
 		children: [
 			{
 				path: "/",
 				element: <Dashboard />,
+			},
+			{
+				path: "/products",
+				element: <Products />,
+			},
+		],
+	},
+	{
+		path: "/",
+		element: <AuthLayout />,
+		children: [
+			{
+				path: "/sign-in",
+				element: <SignIn />,
 			},
 		],
 	},
