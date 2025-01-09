1f901bce3200c66b3f9ec05609c10e69cd2f153d
Ivan Luis Seibel <ivanseibel@gmail.com>
2025-01-09 18:42:59 +0000

    Refactor AppLayout to include navigation and header components

--- src/_layouts/app.tsx
+++ src/_layouts/app.tsx
@@ -1,13 +1,41 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import { Button } from "../components/button";
+import { NavLink } from "../components/nav-link";
+import { User } from "../components/user";
 
 export function AppLayout() {
 	return (
-		<div>
-			<header>
-				<h1>Header</h1>
-			</header>
-			<div>
-				<Outlet />
+		<div className="h-screen bg-background">
+			<div className="flex flex-col h-full w-full max-w-7xl mx-auto">
+				<header className="flex items-center justify-between shadow-sm max-h-20">
+					<div className="flex items-center gap-4">
+						<Link to={"/"}>
+							<img src="logo.svg" alt="" />
+						</Link>
+					</div>
+					<nav className="flex gap-4 flex-1 justify-center">
+						<NavLink icon="ChartHistogramIcon" to={"/"}>
+							Dashboard
+						</NavLink>
+						<NavLink icon="PackageIcon" to={"/products"}>
+							Products
+						</NavLink>
+					</nav>
+					<div className="flex items-center gap-4">
+						<Button
+							label="New product"
+							icon="PlusSignIcon"
+							iconPosition="left"
+							size="sm"
+							variant="solid"
+							onClick={() => {}}
+						/>
+						<User />
+					</div>
+				</header>
+				<div>
+					<Outlet />
+				</div>
 			</div>
 		</div>
 	);
