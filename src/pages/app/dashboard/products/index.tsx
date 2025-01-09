ee2ad3bf0eb305126db20a6cd2cf8fa846963e47
Ivan Luis Seibel <ivanseibel@gmail.com>
2025-01-09 19:07:23 +0000

    Add Products component with SEO title management using Helmet

---
+++ src/pages/app/dashboard/products/index.tsx
@@ -0,0 +1,13 @@
+import { Helmet } from "react-helmet-async";
+
+export function Products() {
+	return (
+		<>
+			<Helmet>
+				<title>Products</title>
+			</Helmet>
+
+			<h1>Products</h1>
+		</>
+	);
+}
