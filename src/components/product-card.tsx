fb1b81a54f287b2b205c7f4f421ec445dd77c969
Ivan Luis Seibel <ivanseibel@gmail.com>
2025-01-13 20:33:15 +0000

    Refactor ProductCard component: wrap card in Link for navigation to product details

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom";
 import { CategoryTag } from "./category-tag";
 import { StatusTag } from "./status-tag";
 
 export interface ProductCardProps {
-	id?: string;
+	id: string;
 	imageUrl: string;
 	title: string;
 	description: string;
@@ -12,6 +13,7 @@ export interface ProductCardProps {
 }
 
 export function ProductCard({
+	id,
 	imageUrl,
 	title,
 	description,
@@ -20,34 +22,36 @@ export function ProductCard({
 	category,
 }: ProductCardProps) {
 	return (
-		<div className="max-w-sm max-h-60 transition-all duration-200 p-1 rounded-[20px] border-2 border-white bg-white hover:border-2 hover:border-blue-base">
-			<div className="relative">
-				<img
-					className="w-full rounded-[20px] h-32 object-cover"
-					src={imageUrl}
-					alt={title}
-				/>
-				<div className="absolute flex gap-2 top-0 right-0 mr-2 mt-2">
-					<StatusTag variant={status} />
-					<CategoryTag label={category} />
+		<Link to={`/product/${id}`} className="block">
+			<div className="max-w-sm max-h-60 transition-all duration-200 p-1 rounded-[20px] border-2 border-white bg-white hover:border-2 hover:border-blue-base">
+				<div className="relative">
+					<img
+						className="w-full rounded-[20px] h-32 object-cover"
+						src={imageUrl}
+						alt={title}
+					/>
+					<div className="absolute flex gap-2 top-0 right-0 mr-2 mt-2">
+						<StatusTag variant={status} />
+						<CategoryTag label={category} />
+					</div>
 				</div>
-			</div>
-			<div className="p-3">
-				<div className="flex justify-between items-center">
-					<h3 className="subtitle text-gray-400">{title}</h3>
-					<div className="flex items-baseline gap-1">
-						<span className="label-md text-gray-500">$</span>
-						<p className="font-dm-sans text-lg font-bold text-gray-500">
-							{price.toFixed(2)}
+				<div className="p-3">
+					<div className="flex justify-between items-center">
+						<h3 className="subtitle text-gray-400">{title}</h3>
+						<div className="flex items-baseline gap-1">
+							<span className="label-md text-gray-500">$</span>
+							<p className="font-dm-sans text-lg font-bold text-gray-500">
+								{price.toFixed(2)}
+							</p>
+						</div>
+					</div>
+					<div>
+						<p className="body-sm text-gray-300 overflow-hidden text-pretty line-clamp-2">
+							{description}
 						</p>
 					</div>
 				</div>
-				<div>
-					<p className="body-sm text-gray-300 overflow-hidden text-pretty line-clamp-2">
-						{description}
-					</p>
-				</div>
 			</div>
-		</div>
+		</Link>
 	);
 }
