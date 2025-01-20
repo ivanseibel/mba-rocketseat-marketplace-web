992599dd0e48a6415614519ffcd75fb6c8401aa1
Ivan Luis Seibel <ivanseibel@gmail.com>
2025-01-20 15:23:55 +0000

    Update Button component layout for improved spacing and alignment

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -24,7 +24,7 @@ export function Button({
 	const IconComponent = icon ? (HugeIcons[icon] as React.ElementType) : null;
 
 	const buttonClasses = cn(
-		"flex w-full items-center gap-2 justify-center font-medium transition-colors duration-200 focus:outline-none",
+		"flex w-full items-center gap-2 justify-between font-medium transition-colors duration-200 focus:outline-none",
 		size === "sm" ? "text-sm" : "text-base",
 		variant === "link"
 			? "p-[2px] text-orange-base hover:text-orange-dark focus:ring-2 focus:ring-orange-base focus:ring-opacity-50" // Estilos de link
