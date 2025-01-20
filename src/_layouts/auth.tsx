c27ac2c4f69def422951ac861878da58b9cc0ef1
Ivan Luis Seibel <ivanseibel@gmail.com>
2025-01-20 15:53:46 +0000

    Refactor AuthLayout component for improved layout and responsiveness

--- src/_layouts/auth.tsx
+++ src/_layouts/auth.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export function AuthLayout() {
 	return (
-		<div className="flex justify-center items-center bg-background w-full h-screen">
-			<aside className="relative w-1/2 h-full">
+		<div className="flex justify-center items-start bg-background w-full min-h-screen">
+			<aside className="relative flex flex-col items-start w-1/2 min-h-screen">
 				<div className="flex p-10">
 					<img src="/logo-full.svg" alt="" />
 				</div>
@@ -11,8 +11,10 @@ export function AuthLayout() {
 					<img src="/background.png" className="w-[755px] h-[497px]" alt="" />
 				</div>
 			</aside>
-			<main className="flex flex-1 bg-white m-24 p-20 rounded-[32px] max-w-[611px]">
-				<Outlet />
+			<main className="flex">
+				<div className="flex flex-1 bg-white m-24 p-20 rounded-[32px] max-w-[611px]">
+					<Outlet />
+				</div>
 			</main>
 		</div>
 	);
