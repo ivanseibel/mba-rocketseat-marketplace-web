8b97c91772e6dc2ca65367927132a563ccbdbf03
Ivan Seibel <me@ivanseibel.dev>
2025-01-25 12:26:40 +0000

    Implement sign-in form with validation and API integration

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,9 +1,45 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "../../components/button";
 import { Input } from "../../components/input";
+import { z } from "zod";
+import { useMutation } from "@tanstack/react-query";
+import { signIn } from "../../api/sign-in";
+import { toast } from "sonner";
+import { useForm, Controller } from 'react-hook-form';
+
+const signInSchema = z.object({
+	email: z.string().email(),
+	password: z.string(),
+});
+
+type SignInFormData = z.infer<typeof signInSchema>;
 
 export function SignIn() {
 	const navigate = useNavigate();
+
+	const { mutateAsync } = useMutation({
+		mutationFn: signIn,
+	});
+
+	const { control, handleSubmit, formState: { isSubmitting} } = useForm<SignInFormData>({
+		defaultValues: {
+			email: "",
+			password: "",
+		}
+	});
+
+	async function handleSignIn(data: z.infer<typeof signInSchema>) {
+		try {
+			await mutateAsync({
+				email: data.email,
+				password: data.password,
+			})
+			navigate("/");
+		} catch (error) {
+			console.error(error);
+			toast.error("Failed to sign in");
+		}
+	}
 
 	function handleSignUp() {
 		navigate("/sign-up");
@@ -11,7 +47,7 @@ export function SignIn() {
 
 	return (
 		<div className="flex flex-col gap-32 w-full">
-			<form className="flex flex-col gap-12 w-full">
+			<form className="flex flex-col gap-12 w-full" onSubmit={handleSubmit(handleSignIn)}>
 				<div className="flex flex-col gap-2">
 					<h1 className="text-gray-500 title-md">Sign in</h1>
 					<p className="text-gray-300 text-sm">
@@ -19,26 +55,41 @@ export function SignIn() {
 					</p>
 				</div>
 				<div className="flex flex-col gap-5 w-full">
-					<Input
-						id="email"
-						type="email"
-						error=""
-						icon="Mail01Icon"
-						label="E-Mail"
-						placeholder="Type your e-mail"
+					<Controller 
+						name="email"
+						control={control}
+						render={({ field }) => (
+							<Input
+								id="email"
+								type="email"
+								error=""
+								icon="Mail01Icon"
+								label="E-Mail"
+								placeholder="Type your e-mail"
+								{...field}
+							/>
+						)}
 					/>
-					<Input
-						id="password"
-						type="password"
-						error=""
-						icon="LockIcon"
-						label="Password"
-						placeholder="Type your password"
+					<Controller 
+						name="password"
+						control={control}
+						render={({ field }) => (
+							<Input
+								id="password"
+								type="password"
+								error=""
+								icon="LockIcon"
+								label="Password"
+								placeholder="Type your password"
+								{...field}
+							/>
+						)}
 					/>
+
 				</div>
 				<Button
 					label="Sign in"
-					onClick={() => {}}
+					type="submit"
 					size="md"
 					variant="solid"
 					icon="ArrowRight02Icon"
@@ -52,6 +103,7 @@ export function SignIn() {
 					onClick={handleSignUp}
 					size="md"
 					variant="outline"
+					disabled={isSubmitting}
 				/>
 			</div>
 		</div>
