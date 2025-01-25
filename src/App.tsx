6ccce824a9c7aea49040f32deea2330ce0e86e7d
Ivan Seibel <me@ivanseibel.dev>
2025-01-25 12:26:33 +0000

    Add Toaster component for notifications in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import queryClient from "./lib/reactquery";
 import { router } from "./routes";
+import { Toaster } from 'sonner'
 
 export function App() {
 	return (
 		<HelmetProvider>
 			<Helmet titleTemplate="%s | Marketplace" />
 			<QueryClientProvider client={queryClient}>
+				<Toaster />
 				<RouterProvider router={router} />
 			</QueryClientProvider>
 		</HelmetProvider>
